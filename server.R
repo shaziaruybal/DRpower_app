898937252b56db5a005c0f264f128bf612f2fe6c
shaziaruybal <shaziaruybal@gmail.com>
2023-06-01 14:49:11 +0100

    first working version

---
+++ server.R
@@ -0,0 +1,358 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(shiny.fluent)
+library(DRpower)
+
+load("dummy_data.RData")
+
+set.seed(10)
+
+function(input, output, session) {
+    # bs_themer()
+  
+  
+  ##################################################
+  # TESTING
+  ##################################################
+  
+  # shiny::observeEvent(input$user_nclust, {
+  #   print("Test button clicked")
+  #   
+  #   plot_data <- data.frame(
+  #     cluster = c(rep(1:input$user_nclust)),
+  #     sample_size = c(rep(100, input$user_nclust)),
+  #     prop_dropout = c(rep(0.1, input$user_nclust))
+  #   )
+  # 
+  #   output$test_plot <- renderPlot({
+  #     ggplot(plot_data, aes(x = cluster, y = sample_size)) +
+  #       geom_point()
+  #   })
+  # })
+  
+  shiny::observeEvent(input$test_button, {
+    plot_data <- rnorm(100)
+    print("Test button clicked")
+
+    output$test_plot <- renderPlot({
+      hist(plot_data, main = "Histogram of Random Data")
+    })
+  })
+
+  ##################################################
+  # DESIGN
+  ##################################################
+  
+  # ----------------------------------
+  # Sample size table
+  # ----------------------------------
+  
+  # TODO: make sure this shows the table representing the correct power selected by the user
+  
+  observeEvent(input$user_pow, {
+    print("Target power selected")
+    
+    if(input$user_pow==0.8)
+      
+      output$sample_size_table <- renderDT(datatable(df_samp_size2, 
+                                                     colnames = c("Number of clusters", "6%", "7%", "8%", "9%", "10%"),
+                                                     rownames = FALSE,
+                                                     extensions = c("Buttons", "FixedHeader"), 
+                                                     options = list(autoWidth = T, 
+                                                                    pageLength = 12,
+                                                                    fixedHeader = TRUE,
+                                                                    columnDefs = list(list(className = "dt-center", 
+                                                                                           targets = "_all")),
+                                                                    searchHighlight = TRUE,
+                                                                    language = list(search = 'Filter:'),
+                                                                    dom = 'rtB',
+                                                                    buttons = c('copy', 'csv', 'excel')))
+      )
+      
+    else
+      output$table_NA <- renderText("There is no table for that target power yet")
+    
+  })
+  
+  # ----------------------------------
+  #  User-input table: sample size and proportion drop-out
+  # ----------------------------------
+  # TODO: n clusters/sample sizes default
+  
+  # get input value from user-specified n clusters
+  observeEvent(input$user_nclust, {
+    print("Number of cluster selected")
+    
+    n_rows <- input$user_nclust
+
+    # create the data frame with fixed columns and rows based on user input
+    df <- data.frame(
+      cluster = c(rep(1:n_rows)),
+      sample_size = c(rep(100, n_rows)),
+      prop_dropout = c(rep(0.1, n_rows))
+    )
+
+    output$user_table <- renderDT({
+      datatable(df,
+                editable = list(
+                  target = 'column',
+                  disable = list(
+                    columns = c(0,3)
+                    )
+                  ),
+                rownames = FALSE,
+                options = list(dom = 'rtip',
+                               autoWidth = TRUE))
+    })
+    
+    # output$output_text <- renderText(paste0("Number of clusters: ", input$user_nclust))
+  })
+
+  # ----------------------------------
+  #  Calculate final sample sizes
+  # ----------------------------------
+  
+  # TODO: add function to calculate final sample size in 3rd column once user has edited the table - maybe have a button to calculate final sample size
+  # need to check how to save table from before with user-entered values
+  
+   observeEvent(input$calc_sizes, {
+    print("Calculate final sample sizes values button clicked")
+    
+     n_rows <- input$user_nclust
+     
+     final_df <- data.frame(
+       cluster = c(rep(1:n_rows)),
+       final_sample_size = c(rep(150, n_rows))
+     )
+     
+    output$final_sizes_table <- renderDT({
+      datatable(final_df, 
+                rownames=F,
+                options = list(dom = 'rti', 
+                               width=4))
+    })
+  })
+  
+  # ----------------------------------
+  #  Results plot: estimated power
+  # ----------------------------------
+  # TODO: store the user-input values from the table and use them as params for DRpower::get_power_threshold()
+  
+  # Display results once estimate power is clicked
+  observeEvent(input$est_pow, {
+    
+    
+    output$title_powbox <- renderText("The estimated power is below: ")
+    output$text_powbox <- renderText(paste0("The plot shows the mean and lower and upper credible interval based on the parameters: ",
+                                            "prev=", input$param_prev, ", ICC=", input$param_icc, ", sims=", input$param_n_sims))
+    
+    # ATM this is hard-coded but will be flexible eventually
+    power_output <- DRpower::get_power_threshold(N = c(rep(100, 10)),
+                                                 prevalence = 0.06,
+                                                 ICC = 0.1,
+                                                 reps = 100
+                                                )
+    
+    output$est_power_plot <- renderPlot({
+      ggplot(power_output) +
+        geom_segment(aes(x = " ", xend = " ",y = lower, yend = upper), color = "black", linewidth = 1) +
+        geom_point(aes(x = " ", y = power),
+                   size = 4,
+                   shape = 21,
+                   fill = "skyblue3") +
+        scale_y_continuous(labels = scales::percent_format(1), limits = c(0,1)) +
+        labs(x = "",
+             y = "Estimated power",
+             caption = paste0("Parameters: prev=", input$param_prev, ", ICC=", input$param_icc, ", sims=", input$param_n_sims)) +
+        theme_light() +
+        theme(text = element_text(size = 16))
+    })
+  })
+  
+  # ----------------------------------
+  #  Render downloadable design report
+  # ----------------------------------
+  # TODO: fix the reactive plot issue 
+  
+  output$design_report <- downloadHandler(
+    filename = paste0("PfHRP2_Planner_Design_Report_", Sys.Date(), ".html"),
+    content = function(file) {
+      tempReport <- file.path(tempdir(), "test_design_report.Rmd")
+      file.copy("test_design_report.Rmd", tempReport, overwrite = TRUE)
+      
+      params <- list(est_prev_plot = est_prev_plot(),
+                     est_icc_plot = est_icc_plot())
+      
+      rmarkdown::render(tempReport,
+                        output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv()),
+      )
+    }
+  )
+
+  ##################################################
+  # ANALYSIS
+  ##################################################
+  
+  # ----------------------------------
+  #  User-input table: number of deletions and final sample sizes
+  # ----------------------------------
+
+  # get input value from user-specified n clusters
+  observeEvent(input$analysis_nclust, {
+    print("Number of final clusters selected")
+    
+    n_rows <- input$analysis_nclust
+    
+    # create the data frame with fixed columns and rows based on user input
+    analysis_table <- data.frame(
+      cluster = c(rep(1:n_rows)),
+      n_deletions = c(rep(5, n_rows)),
+      sample_size = c(rep(100, n_rows))
+    )
+    
+    output$analysis_table <- renderDT({
+      datatable(analysis_table, 
+                editable = list(
+                  target = 'column',
+                  disable = list(
+                    columns = c(0)
+                  )
+                ),
+                rownames = FALSE,
+                options = list(dom = 'rtip',
+                               autoWidth = TRUE)) 
+  
+
+  })
+  }) 
+  
+  # ----------------------------------
+  #  Results table/plot: estimated prevalence
+  # ----------------------------------
+  
+  # Display results once estimate prevalence is clicked
+  observeEvent(input$est_prev, {
+    print("Estimate prevalence button clicked")
+    
+    # df_prev_output <- df_prev_output %>% select(simple_mean, lower_CrI, upper_CrI, prob_above_threshold)
+    
+    output$title_prevbox <- renderText("The estimated prevalence value is below: ")
+    output$text_prevbox <- renderText("The table and plot show the mean and lower and upper credible interval")
+    
+    # ATM this is hard-coded but will be flexible eventually
+    prev_output <- DRpower::get_prevalence(n = c(rep(5, 10)), N = c(rep(100, 10)))
+    
+    output$est_prev_table <- renderDT({
+      datatable(prev_output, 
+                rownames = FALSE, 
+                colnames = c("Mean prevalence", "Lower CrI", "Upper CrI", "Probability above threshold"),
+                options = list(autoWidth = TRUE, 
+                               fixedHeader = TRUE,
+                               columnDefs = list(list(className = "dt-center", 
+                                                      targets = "_all")),
+                               dom = 't'))
+    })
+    
+    # NOTE need to divide by 100 to convert to proportion
+    
+    est_prev_plot <- reactive({
+      ggplot(prev_output) +
+        geom_segment(aes(x = " ", xend = " ", y = CrI_lower/100, yend = CrI_upper/100), 
+                     color = "black", linewidth = 1) +
+        geom_point(aes(x = " ", y = MAP/100),
+                   size = 3,
+                   shape = 21,
+                   fill = "skyblue3") +
+        # geom_hline(aes(y=0.06),
+        #            color = "darkgrey",
+        #            linetype = "dashed") +
+        scale_y_continuous(labels = scales::percent_format(1), limits = c(0,1)) +
+        labs(x = "",
+             y = "Estimated prevalence") +
+        # coord_flip() +
+        theme_light() +
+        theme(text = element_text(size = 16))
+    })
+    
+    output$est_prev_plot <- renderPlot(est_prev_plot())
+    
+  })
+  
+  # ----------------------------------
+  #  Results table/plot: estimated ICC
+  # ----------------------------------
+  
+  observeEvent(input$est_icc, {
+    print("Estimate ICC button clicked")
+    
+    df_prev_output <- df_prev_output %>% select(simple_mean, lower_CrI, upper_CrI, prob_above_threshold)
+    
+    output$title_iccbox <- renderText("The estimated ICC value is below: ")
+    output$text_iccbox <- renderText("The table and plot show the mean and lower and upper credible interval")
+    
+    output$est_icc_table <- renderDT({
+      datatable(df_prev_output,
+                rownames = FALSE,
+                options = list(autoWidth = TRUE,
+                               fixedHeader = TRUE,
+                               columnDefs = list(list(className = "dt-center",
+                                                      targets = "_all")),
+                               dom = 't'))
+    })
+  
+    est_icc_plot <- reactive({
+      ggplot() +
+        geom_segment(aes(x = " ", xend = " ", y = 0.2, yend = 0.6), 
+                     linewidth = 2,
+                     color = "darkgrey",
+                     alpha = 0.8) +
+        geom_point(aes(x = " ", y = 0.46), 
+                   size = 4, 
+                   shape = 21,
+                   fill = "skyblue3") +
+        scale_y_continuous(labels = scales::percent_format(1), limits = c(0,1)) +
+        labs(x = "",
+             y = "Estimated ICC") +
+        # coord_flip() +
+        theme_light() +
+        theme(text = element_text(size = 16)) 
+    })
+  
+    output$est_icc_plot <- renderPlot(est_icc_plot())
+  })
+  
+  # ----------------------------------
+  #  Render downloadable analysis report  
+  # ----------------------------------
+  # TODO: fix the reactive plot issue 
+  
+  output$analysis_report <- downloadHandler(
+    filename = paste0("PfHRP2_Planner_Analysis_Report_", Sys.Date(), ".html"),
+    content = function(file) {
+      tempReport <- file.path(tempdir(), "test_design_report.Rmd")
+      file.copy("test_design_report.Rmd", tempReport, overwrite = TRUE)
+      
+      params <- list(est_prev_plot = est_prev_plot(),
+                     est_icc_plot = est_icc_plot())
+      
+      rmarkdown::render(tempReport,
+                        output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv()),
+      )
+    }
+  )
+  
+  
+}
---
+++ ui.R
@@ -0,0 +1,255 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(DT)
+library(shiny.fluent)
+library(shiny.blueprint)
+library(shinydashboard)
+
+dashboardPage(#theme = "flatly",
+  dashboardHeader(title = "pfhrp2 Planner"),
+  dashboardSidebar(width = 150,
+                   # size = "thin",
+                   sidebarMenu(
+                    # menuItem(tabName = "test_tab",
+                    #          text = "TESTING"),
+                    menuItem(tabName = "about", 
+                             text = " About",
+                             icon = icon("home")
+                    ),
+                    menuItem(tabName = "design", 
+                             text = " Design", 
+                             icon = icon("paintbrush")
+                    ),
+                    menuItem(tabName = "analysis",
+                             text = " Analysis",
+                             icon = icon("chart-line")
+                    )
+  )),
+  dashboardBody(
+    tabItems(
+      # ----------------------------------
+      # TESTING
+      # tabItem(tabName = "test_tab",
+      #         actionButton("test_button", "Test me"),
+      #         plotOutput("test_plot")
+      #         ),
+      # ----------------------------------
+      # Tab 1 - About
+      tabItem(
+        tabName = "about",
+        fluidRow(
+          column(width = 12, style='padding:20px;',
+                 Callout(
+                   title = "How to use this tool",
+                   "Some intro text here and the aims of this tool. Basic instructions for how to use are found below. ",
+                   "For more detailed information, see the DRpower R package website"
+                 )
+          )
+        )
+      ),
+      # ----------------------------------
+      # Tab 2 - Design
+      tabItem(
+        tabName = "design",
+        fluidRow(
+          mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Sample size tables",
+                                 br(),
+                                 box(width = 10, 
+                                     title = "Sample sizes to achieve a given target power", 
+                                     TooltipHost(content = "TEST: Click and select the row with the number of clusters you intend to use for your study",
+                                                 delay = 0,
+                                                 Text(htmltools::em("TEST: Interact with the table below"))
+                                     ),
+                                     br(),
+                                     selectInput(
+                                       inputId = "user_pow",
+                                       label = "Target power: ", 
+                                       width = "auto",
+                                       choices = c(seq(0.0, 1.0, by = 0.1)),
+                                       selected = 0.5
+                                     ),
+                                     DTOutput("sample_size_table"),
+                                     verbatimTextOutput("table_NA")
+                                 )
+                        ),
+                        tabPanel("Final cluster sizes",
+                                 tags$head(tags$script(src = "message-handler.js")),
+                                 br(),
+                                 fluidRow(
+                                   box(width = 7,
+                                       title = "1. Enter the values specific to your study", 
+                                       TooltipHost(content = "Select the number of clusters in your study so that you can populate the table with your sample sizes and the estimated proportion of study participant drop-out.",
+                                                   delay = 0,
+                                                   Text(htmltools::em("Select the number of clusters in your study"))
+                                       ),
+                                       selectInput(
+                                         inputId = "user_nclust",
+                                         label = strong("Select number of clusters: "),
+                                         width = "auto",
+                                         choices = c(1, 2, 3, 4, 5, 6,7, 8, 9, 10, 15, 20), 
+                                         selected = 10,
+                                       ),
+                                       br(),
+                                       # verbatimTextOutput("output_text"), # this is a test to check what cluster integer is selected
+                                       DTOutput("user_table"),
+                                       br(),
+                                       actionButton(
+                                         inputId = "calc_sizes",
+                                         label = "Calculate final sample sizes")
+                                   ),
+                                   box(width = 5, 
+                                       background = "purple",
+                                       title = "Placeholder",
+                                       "maybe the final sample size table can go here?",
+                                       DTOutput("final_sizes_table")
+                                       ),
+                                 ),
+                                 fluidRow(
+                                   box(
+                                     width = 7,
+                                     title = "2. Estimate power", 
+                                     TooltipHost(content = "Select the parameters.",
+                                                 delay = 0,
+                                                 Text(htmltools::em("Select the parameters"))
+                                     ),
+                                     selectInput("param_prev",
+                                                 label = "Select prevalence:",
+                                                 choices = c(0.06, 0.07, 0.08, 0.09, 0.10), 
+                                                 selected = "0.06",
+                                                 width = "200px",
+                                     ),
+                                     selectInput("param_icc", 
+                                                 label = "Select intra-cluster correlation:",
+                                                 choices = c(0.01, 0.05, 0.10), 
+                                                 selected = "0.1",
+                                                 width = "200px",
+                                     ),
+                                     selectInput("param_n_sims", 
+                                                 label = "Select the number of simulations:",
+                                                 choices = c(seq(100, 1000, by=100)), 
+                                                 selected = "100",
+                                                 width = "250px",
+                                     ),
+                                     actionButton(inputId = "est_pow",
+                                                  label = "Estimate power",
+                                                  icon = icon("clipboard-check"))
+                                   ),
+                                   box(width = 5,
+                                       background = "purple",
+                                       title = textOutput("title_powbox"),
+                                       p(textOutput("text_powbox")),
+                                       br(),
+                                       plotOutput("est_power_plot")
+                                   ),
+                                 )
+                        ),
+                        tabPanel(title = "Generate report",
+                                 br(),
+                                 Callout(
+                                   title = "Download the design phase report",
+                                   "Click the button below to generate report. This creates a pdf or similar with standardised text describing ",
+                                   "the assumptions set previously. This provides text that can be copied over directly into a paper/report to minimise mistakes",
+                                   br(), br(),
+                                   downloadButton("design_report", "Download design report", icon("download"))
+                                 ),
+                        )
+            )
+          )
+        )
+      ),
+      # ----------------------------------
+      # Tab 3 - Analysis
+      tabItem(
+        tabName = "analysis",
+        fluidRow(
+          mainPanel(
+            tabsetPanel(
+              type = "tabs",
+              tabPanel(
+                title = "Estimate prevalence",
+                fluidRow(
+                  column(width = 12, style='padding:20px;',
+                         Callout(
+                           title = "Estimate prevalence",
+                           "To estimate the prevalence of *pfhrp2* deletions, enter the final values obtained from the study in the table below. ",
+                           "These parameters are used for inference and to produce various estimates of the prevalence, including simple mean, ",
+                           "posterior mean and median, the credible interval (CrI)."
+                         )
+                  ),
+                  box(width = 10,
+                      title = "1. Enter the values specific to your study",
+                      br(),
+                      selectInput(
+                        inputId = "analysis_nclust",
+                        label = strong("Select final number of clusters: "),
+                        width = "auto",
+                        choices = c(1, 2, 3, 4, 5, 6,7, 8, 9, 10, 15, 20), 
+                        selected = 10,
+                      ),
+                      DTOutput("analysis_table"),
+                      br(),
+                      actionButton(inputId = "est_prev", 
+                                   label = "Estimate prevalence", 
+                                   icon("clipboard-check")),
+                  )
+                ),
+                box(width = 10, background = "purple",
+                    title = textOutput("title_prevbox"),
+                    p(textOutput("text_prevbox")),
+                    br(),
+                    DTOutput("est_prev_table"),
+                    br(),
+                    plotOutput("est_prev_plot")
+                ),
+              ),
+              tabPanel(
+                title= "Estimate ICC",
+                fluidRow(
+                column(width = 12, style='padding:20px;',
+                       Callout(
+                         title = "Estimate ICC",
+                         "Supplementary analyses include calculating ICC.... bla bla ",
+                         br(),
+                         actionButton(inputId = "est_icc",
+                                      label = "Estimate ICC",
+                                      icon("clipboard-check"))
+                       )
+                ),
+                ),
+                box(width = 10, background = "purple",
+                      title = textOutput("title_iccbox"),
+                      p(textOutput("text_iccbox")),
+                      br(),
+                      DTOutput("est_icc_table"),
+                      br(),
+                      plotOutput("est_icc_plot")
+                ),
+              ),
+              tabPanel(
+                title = "Generate report",
+                br(),
+                Callout(
+                  title = "Download the analysis phase report",
+                  "Click the button below to generate report. This creates a pdf or similar with standardised text describing ",
+                  "the assumptions set previously. This provides text that can be copied over directly into a paper/report to minimise mistakes",
+                  br(), br(),
+                  downloadButton("analysis_report", "Download analysis report", icon("download"))
+                ),
+              )
+            )
+          )
+        )
+      )
+    )
+  ), skin = "purple"
+)
